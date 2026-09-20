fd01fcad4b9cfa478ed63d7b653d7e731adc0cd7
Dan Hart <dshart@gmail.com>
2026-09-20 11:12:32 -0400

    Get all games endpoint

--- src/GameStore.Api/Program.cs
+++ src/GameStore.Api/Program.cs
@@ -1,6 +1,32 @@
+using GameStore.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<Game> games =
+[
+    new Game {
+        Id = Guid.NewGuid(),
+        Name = "Donkey Kong",
+        Genre = "Arcade",
+        Price = 19.99m,
+        ReleaseDate = new DateOnly(1982, 7, 15)
+    },
+    new Game {
+        Id = Guid.NewGuid(),
+        Name = "Lego Villains",
+        Genre = "Family",
+        Price = 59.99m,
+        ReleaseDate = new DateOnly(2010, 9, 30) },
+    new Game {
+        Id = Guid.NewGuid(),
+        Name = "FIFA 23",
+        Genre = "Sports",
+        Price = 69.99m,
+        ReleaseDate = new DateOnly(2022, 9, 27) }
+];
+
+// GET /games
+app.MapGet("/games", () => games);
 
 app.Run();
