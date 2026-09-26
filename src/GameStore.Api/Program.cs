31ca25a32b940a4e1a7928b5797339222a8bc289
Dan Hart <dshart@gmail.com>
2026-09-26 09:30:25 -0400

    added Genre model and changed code that uses it

--- src/GameStore.Api/Program.cs
+++ src/GameStore.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using GameStore.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,30 +7,45 @@ builder.Services.AddValidation();
 
 var app = builder.Build();
 
+const string GetGameEndpointName = "GetGame";
+
+List<Genre> genres =
+[
+    new Genre { Id = new Guid("4e179397-c3f1-45ec-a271-c26f07ff64f3"), Name = "Fighting"},
+    new Genre { Id = new Guid("b2c3d4e5-f678-90a1-b2c3-d4e5f67890a1"), Name = "Kids and Family" },
+    new Genre { Id = new Guid("c3d4e5f6-7890-a1b2-c3d4-e5f67890a1b2"), Name = "Racing" },
+    new Genre { Id = new Guid("d4e5f678-90a1-b2c3-d4e5-f67890a1b2c3"), Name = "Roleplaying" },
+    new Genre { Id = new Guid("e5f67890-a1b2-c3d4-e5f6-7890a1b2c3d4"), Name = "Sports" }
+];
+
 List<Game> games =
 [
     new Game {
         Id = Guid.NewGuid(),
         Name = "Donkey Kong",
-        Genre = "Arcade",
+        Genre = genres[0],
         Price = 19.99m,
-        ReleaseDate = new DateOnly(1982, 7, 15)
+        ReleaseDate = new DateOnly(1982, 7, 15),
+        Description = "Donkey Kong[a] is a video game series and media franchise created by the Japanese game designer Shigeru Miyamoto for Nintendo. It follows the adventures of Donkey Kong, a large, powerful gorilla, and other members of the Kong family of simians.."
     },
     new Game {
         Id = Guid.NewGuid(),
         Name = "Lego Villains",
-        Genre = "Family",
+        Genre = genres[1],
         Price = 59.99m,
-        ReleaseDate = new DateOnly(2010, 9, 30) },
-    new Game {
+        ReleaseDate = new DateOnly(2010, 9, 30),
+        Description = "Smash legos and grab gold bricks as the Joker."
+    },
+    new Game
+    {
         Id = Guid.NewGuid(),
         Name = "FIFA 23",
-        Genre = "Sports",
+        Genre = genres[2],
         Price = 69.99m,
-        ReleaseDate = new DateOnly(2022, 9, 27) }
+        ReleaseDate = new DateOnly(2022, 9, 27),
+        Description = "FIFA 23 is a football video game published by EA Sports. It is the 30th and final installment in the FIFA series that is developed by EA Sports"
+    }
 ];
-
-const string GetGameEndpointName = "GetGame";
 
 // GET /games
 app.MapGet("/games", () => games);
@@ -69,6 +85,8 @@ app.MapPut("/games/{id}", (Guid id, Game updatedGame) =>
     existingGame.Genre = updatedGame.Genre;
     existingGame.Price = updatedGame.Price;
     existingGame.ReleaseDate = updatedGame.ReleaseDate;
+    existingGame.Description = updatedGame.Description;
+
 
     return Results.NoContent();
 });
